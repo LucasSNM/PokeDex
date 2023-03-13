c80ef86e42619800116058de47202ecc7833666d
Lucas Moreira <lucas.moreira@aptiv.com>
2023-03-13 07:39:27 -0300

    att

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react';
-import { useNavigate } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 import styles from "./Home.module.css";
 
@@ -12,62 +12,64 @@ import styles from "./Home.module.css";
 // }
 
 function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 export function Home() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [listPokemons, setListPokemons] = useState([]);
 
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [listPokemons, setListPokemons] = useState([]);
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/?limit=20`)
+      .then((res) => res.json())
+      .then(
+        (data) => {
+          setListPokemons(data);
+          // if (index == limit - 1)
+          setIsLoaded(true);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, []);
 
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Carregando...</div>;
+  } else {
+    $.adaptiveBackground.run();
 
-    useEffect(() => {
-        fetch(`https://pokeapi.co/api/v2/pokemon/?limit=20`)
-            .then(res => res.json())
-            .then(
-                (data) => {
-                    setListPokemons(data);
-                    // if (index == limit - 1)
-                    setIsLoaded(true)
-                },
-                (error) => {
-                    setIsLoaded(true);
-                    setError(error);
-                }
-            )
-    }, [])
-
-    if (error) {
-        return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-        return <div>Carregando...</div>;
-    } else {
-
-        $.adaptiveBackground.run();
-
-        return (
-            <div className={styles.pokemonCardContainer}>
-                <header>
-                    <img src={`../../pokeball_icon.svg`} height='100px' />
-                </header>
-                <br />
-                {
-                    listPokemons.results.map(pokemon => (
-                        <a className={styles.pokemonCard} href={`/poke?pokemon=${pokemon.name}`}>
-                            <img
-                                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.url.split('/')[6]}.png`}
-                                data-adaptive-background
-                            />
-                            <div className={styles.pokemonInfo}>
-                                <span className={styles.pokemonNumber}>#{pokemon.url.split('/')[6].padStart(3, '0')}</span>
-                                <span>{capitalizeFirstLetter(pokemon.name)}</span>
-                            </div>
-                        </a>
-                    ))}
+    return (
+      <div className={styles.pokemonCardContainer}>
+        <header>
+          <img src={`../../pokeball_icon.svg`} height="100px" />
+        </header>
+        <br />
+        {listPokemons.results.map((pokemon) => (
+          <a
+            key={`card-${pokemon.name}`}
+            className={styles.pokemonCard}
+            href={`/poke?pokemon=${pokemon.name}`}
+          >
+            <img
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${
+                pokemon.url.split("/")[6]
+              }.png`}
+              data-adaptive-background
+            />
+            <div className={styles.pokemonInfo}>
+              <span className={styles.pokemonNumber}>
+                #{pokemon.url.split("/")[6].padStart(3, "0")}
+              </span>
+              <span>{capitalizeFirstLetter(pokemon.name)}</span>
             </div>
-        );
-    }
-
-
+          </a>
+        ))}
+      </div>
+    );
+  }
 }
--- src/Pages/Poke.jsx
+++ src/Pages/Poke.jsx
@@ -1,70 +1,77 @@
-import { useEffect, useState } from 'react'
-import { useLocation } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 import styles from "./Poke.module.css";
 
 function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 export function Poke() {
+  const location = useLocation();
+  const queryParameters = new URLSearchParams(location.search);
+  let pokemonChosed = queryParameters.get("pokemon");
 
-    const location = useLocation()
-    const queryParameters = new URLSearchParams(location.search)
-    let pokemonChosed = queryParameters.get("pokemon")
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [pokemon, setPokemon] = useState([]);
 
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [pokemon, setPokemon] = useState([]);
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonChosed}`)
+      .then((res) => res.json())
+      .then(
+        (data) => {
+          setPokemon(data);
+          setIsLoaded(true);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, []);
 
-    useEffect(() => {
-        fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonChosed}`)
-            .then(res => res.json())
-            .then(
-                (data) => {
-                    setPokemon(data);
-                    setIsLoaded(true)
-                },
-                (error) => {
-                    setIsLoaded(true);
-                    setError(error);
-                }
-            )
-    }, [])
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Carregando...</div>;
+  } else {
+    $.adaptiveBackground.run();
 
-    if (error) {
-        return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-        return <div>Carregando...</div>;
-    } else {
-
-        $.adaptiveBackground.run();
-
-        return (
-            <div className={styles.Poke}>
-                {/* <h1>#{pokemon.id.padStart(3,'0')}</h1> */}
-                <div className={styles.Name}>
-                    <h1>#{pokemon.id}</h1>
-                    <h1>{capitalizeFirstLetter(pokemon.name)}</h1>
-                </div>
-                <img
-                    src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`}
-                    data-adaptive-background
-                />
-                <div className={styles.Description}>
-                    <p>
-                        <p>Altura: {pokemon.height}</p>
-                        <p>Largura: {pokemon.weight}</p>
-                        {/* {listPokemons.types.map(type => (<p>1</p>))} */}
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore at iusto laborum suscipit illum nemo animi iure voluptatum iste reprehenderit odio minima necessitatibus doloremque sunt, voluptates provident ullam, ipsam similique.
-                    </p>
-                </div>
-            </div>
-
-        );
-    }
+    return (
+      <div className={styles.Poke}>
+        {/* <h1>#{pokemon.id.padStart(3,'0')}</h1> */}
+        <div className={styles.Name}>
+          <h1>#{pokemon.id}</h1>
+          <h1>{capitalizeFirstLetter(pokemon.name)}</h1>
+        </div>
+        <img
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`}
+          data-adaptive-background
+        />
+        <div className={styles.Description}>
+          <p>Altura: {pokemon.height}</p>
+          <p>Largura: {pokemon.weight}</p>
+          <hr />
+          <p>
+            {pokemon.stats.map((stat, index) => (
+              <p key={`stat-${stat.stat.name}-${stat.base_stat}`}>
+                {stat.stat.name}: {stat.base_stat}
+              </p>
+            ))}
+          </p>
+          <hr />
+          {pokemon.types.map((type, index) => (
+            <button key={`type-${type.type.name}`}>{type.type.name}</button>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
-{/* <img src={pokemon.sprites.front_default}/>
+{
+  /* <img src={pokemon.sprites.front_default}/>
 <img src={pokemon.sprites.other.dream_world.front_default}/>
-<img src={pokemon.sprites.other.home.front_default}/> */}
+<img src={pokemon.sprites.other.home.front_default}/> */
+}
